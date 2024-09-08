74419cb6373dbbad08c272871da782e067c39502
Mateus Mourao <mateusvmourao@gmail.com>
2024-09-08 14:23:44 -0300

    criando repositório

---
+++ Target.BE/Program.cs
@@ -0,0 +1,15 @@
+﻿// Valores de faturamento
+decimal[] faturamentoDiario = [0, 1500, 2000, 0, 0, 2500, 3000, 0, 3200, 500, 0, 4000];
+
+// Remove dias com faturamento 0
+var diasComFaturamento = faturamentoDiario.Where(faturamento => faturamento > 0).ToArray();
+
+// Calcula as métricas
+decimal menorFaturamento = diasComFaturamento.Min();
+decimal maiorFaturamento = diasComFaturamento.Max();
+decimal mediaAnual = diasComFaturamento.Average();
+int diasAcimaDaMedia = diasComFaturamento.Count(faturamento => faturamento > mediaAnual);
+
+Console.WriteLine($"Menor faturamento: {menorFaturamento}");
+Console.WriteLine($"Maior faturamento: {maiorFaturamento}");
+Console.WriteLine($"Dias com faturamento acima da média: {diasAcimaDaMedia}");
